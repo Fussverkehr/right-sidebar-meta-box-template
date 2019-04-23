6aeac7e9f16b8bd4610e988177ec3b75afa69f81
Fussverkehr <dominik.bucheli@fussverkehr.ch>
2019-04-23 13:55:00 +0200

    Update custom-meta-box-template.php

--- custom-meta-box-template.php
+++ custom-meta-box-template.php
@@ -8,6 +8,7 @@ Description: Enables TinyMCE for the richt_sidebar meta.
 Author: Dominik Bucheli Fussverkehr Schweiz
 Version: 1.0
 Author URI: http://fussverkehr.ch/
+GitHub Plugin URI: https://github.com/Fussverkehr/right-sidebar-meta-box-template
 */
 
 
